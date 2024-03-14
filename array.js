ee128d4fccd2e31d11d2c9457aad62c4dd9bbaf8
luschettini <luizaschettinii@gmail.com>
2024-03-14 17:13:10 -0300

    push

---
+++ array.js
@@ -0,0 +1,16 @@
+let animais = ['tartaruga', 'macaco']
+console.log("-------Lista Inicial--------");
+console.log("animais");
+console.log(animais.length);
+
+animais .push('arara');
+animais .push('zebra');
+animais .push('girafa');
+animais .push('hipopótamo');
+
+
+console.log("------Lista Final------");
+console.log(animais);
+
+// O Método push permite adicionar um ou mais elementos ao final do Array existente. 
+
