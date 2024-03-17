f8dd0b9d45464268a09d40326a2b27fcd1de8f28
Luiza <luizaschettinii@gmail.com>
2024-03-17 19:24:08 -0300

    Método Array pop

---
+++ pop.js
@@ -0,0 +1,14 @@
+// Método .Pop
+let convidados = ['Julia', 'Isadora', 'Ana', 'Felipe'];
+console.log(convidados);
+
+let ultimoConvidado = convidados.pop();
+console.log(ultimoConvidado);
+console.log(convidados);
+
+let tarefas = ['Estudar', 'Lavar louça', 'Passear com o cachorro', 'Limpar quarto' ];
+console.log (tarefas);
+
+let ultimaTarefa = tarefas.pop();
+console.log(ultimaTarefa);
+console.log(tarefas);
