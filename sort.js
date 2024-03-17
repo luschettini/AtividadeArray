a0cce3100f408049b4d767ac3050493f4f5eb297
Luiza <luizaschettinii@gmail.com>
2024-03-17 20:01:00 -0300

    Método array sort

---
+++ sort.js
@@ -0,0 +1,23 @@
+//Método sort.js
+
+//ex. 01
+
+//let frutas (array)
+let frutas = ['cereja', 'mirtilo', 'lichia'];
+
+//O sort é usado para ordenar os elementos do array em ordem alfabética.
+frutas.sort();
+console.log(frutas);
+
+//Saída esperada:[ 'cereja', 'lichia', 'mirtilo' ].
+
+//ex. 02
+
+//let nomes (array)
+let nomes = ['Liz', 'Maya', 'Catarina', 'Lucca'];
+
+//O sort é usado para ordenar os elementos do array em ordem alfabética.
+nomes.sort();
+console.log("Lista alfabética", nomes);
+
+//Saída esperada: Lista alfabética [ 'Catarina', 'Liz', 'Lucca', 'Maya' ].
