c33b5404576fb248141ef95a5099602394a939ba
Luiza <luizaschettinii@gmail.com>
2024-03-17 19:07:25 -0300

    Ajuste nos exemplos

--- array.js
+++ array.js
@@ -1,16 +1,36 @@
-let animais = ['tartaruga', 'macaco']
-console.log("-------Lista Inicial--------");
-console.log("animais");
-console.log(animais.length);
+// Método push.js
 
-animais .push('arara');
-animais .push('zebra');
-animais .push('girafa');
-animais .push('hipopótamo');
+// ex. 01
+// Um array chamado paises é inicializado com 3 elementos: 'Australia', 'Chile', 'Estados Unidos'.
+let paises = ["Australia", "Chile", "Estados Unidos"];
+console.log("-----Lista Inicial-----");
+console.log("paises");
+console.log(paises.length);
 
+// O método push adiciona um ou mais elementos ao final de um array e aumenta o seu tamanho e insire os novos elementos no final. Inseri 3 novos elementos: 'Turquia', 'Italia', 'Espanha'.
 
-console.log("------Lista Final------");
-console.log(animais);
+paises.push("Turquia");
+paises.push("Italia");
+paises.push("Espanha");
 
-// O Método push permite adicionar um ou mais elementos ao final do Array existente. 
+console.log("-----Lista Final-----");
+console.log(paises);
+
+//ex. 02
+// Um array chamado paises é inicializado com 3 elementos: 'Macarrão', 'Lasanha', 'Cappelletti'.
+let massas = ["Macarrão", "Lasanha", "Cappelletti"];
+console.log("-----Lista Inicial-----");
+console.log("massas");
+console.log(massas.length);
+
+// O método push adiciona um ou mais elementos ao final de um array e aumenta o seu tamanho e insire os novos elementos no final. Inseri 3 novos elementos: 'Ravioli', 'Mezzelune', 'Cannelloni'.
+
+paises.push("Ravioli");
+paises.push("Mezzelune");
+paises.push("Cannelloni");
+
+console.log("-----Lista Final-----");
+console.log(massas);
+
+// O Método push permite adicionar um ou mais elementos ao final do Array existente. Ele modifica o próprio array, aumentando o seu tamanho e inserindo os novos elementos ao final. É possível adicionar múltiplos elementos ao array de uma só vez.
 
