910137d6aacb09fd39e03ab2f874e3c0de8a4d4c
Luiza <luizaschettinii@gmail.com>
2024-03-17 21:26:13 -0300

    Método array shift

---
+++ shift.js
@@ -0,0 +1,19 @@
+//Método shift
+
+//ex. 01
+//let flores (array)
+let flores = ['rosa', 'tulipa', 'peonia'];
+
+//Determinar se é true or false, ver se a flor de girassol está inclusa na lista.
+console.log(flores.includes("girassol")); 
+//Saída esperada: false.
+
+//O includes é utilizado quando for preciso saber se o array ou objeto possui determinado valor ou elemento.
+
+//ex. 02
+//let nome (array)
+let nome = ['Felipe', 'Francisco', 'João'];
+
+//Determinar se é true or false, para ver se o nome Felipe está na lista.
+console.log(nome.includes("Felipe"));
+//Saída esperada: true.
